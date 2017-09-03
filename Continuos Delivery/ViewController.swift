262d2dd503e7f3a8f2497068c28fc0ce6d8902db
Adam Rush <adam@adr-studio.co.uk>
2017-09-03 22:43:58 +0100

    Changing subtitle to say Adam Rush is cool

--- Continuos Delivery/ViewController.swift
+++ Continuos Delivery/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.mainLabel.text = "Adam Rush"
+        self.mainLabel.text = "Adam Rush ia cool"
     }
 
     override func didReceiveMemoryWarning() {
