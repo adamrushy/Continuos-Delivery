f33d181d72106871d1169aebc8d7b5f5162a926a
Adam Rush <adam@adr-studio.co.uk>
2017-09-03 22:52:02 +0100

    Fixing label

--- Continuos Delivery/ViewController.swift
+++ Continuos Delivery/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.mainLabel.text = "Adam Rush ia cool"
+        self.mainLabel.text = "Adam Rush"
     }
 
     override func didReceiveMemoryWarning() {
